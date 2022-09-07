8a9b78d0640257f9724175529c5c1eb12045699e
Tomasz Kalinowski <tomasz@rstudio.com>
2022-09-07 12:26:11 -0400

    support batch runs

--- R/guild-calls.R
+++ R/guild-calls.R
@@ -10,12 +10,24 @@ ls_runs <- function() {
 
 #' @export
 guild_run <- function(file = "train.R", flags = NULL, echo = TRUE) {
+  # echo not supported yet, see "Q's for Garrett"
+  if(is.data.frame(flags)) {
+    for(r in seq_len(nrow(flags)))
+      guild_run(file, unclass(flags[r,]), echo)
+    return()
+    # TODO: writeout flags to tempfile csv/json/yaml, supply to
+    # guild call like: `guild run '@/path/to/tmpdir/tmpfile.json`
+  }
+
   if(!is.null(flags))
     flags <- shQuote(sprintf("%s=%s", names(flags), as.character(flags)))
-  guild("run", "--yes",  file, flags) #if(echo) "--echo",
+  guild("run", "--yes", file, flags) #if(echo) "--echo",
 }
 
 
+
+
 guild_view <- function() {
+  # use processx here?
   guild("view", wait = FALSE)
 }
