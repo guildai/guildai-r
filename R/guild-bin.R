2e71be463675b589cd46cc5f80f428dcd060a3ce
Tomasz Kalinowski <tomasz@rstudio.com>
2022-09-23 10:42:50 -0400

    default to using isolated stand-alone guild installation

--- R/guild-bin.R
+++ R/guild-bin.R
@@ -2,10 +2,16 @@
 
 find_python <- function() {
   # consider using pipenv or similar to make a truly stand-alone installation
-  if(file.exists(python <- Sys.which("python3")))
-    return(as.vector(python))
-  if(file.exists(python <- Sys.which("python")))
-    return(as.vector(python))
+
+  for (python in as.vector(c(
+    "/usr/bin/python3",
+    Sys.which("python3"),
+    Sys.which("python")
+  ))
+  )
+    if(file.exists(python))
+      return(python)
+
   stop(
 "python executable not found. Please:
   -  download and install Python http://python.org/downloads/
@@ -13,20 +19,26 @@ find_python <- function() {
 }
 
 
-install_guild <- function() {
+#' @export
+install_guild <- function(python = NULL, guildai = NULL) {
   venv <- normalizePath(rappdirs::user_data_dir("r-guildai"), mustWork = FALSE)
   unlink(venv, recursive = TRUE)
-  system2(find_python(), c("-m", "venv", shQuote(venv)))
+  system2(python %||% find_python(), c("-m", "venv", shQuote(venv)))
   py <- file.path(venv, "bin", if(xfun::is_windows()) "python.exe" else "python")
-  system2(py, c("-m", "pip", "install", "--upgrade", "--no-user", "pip", "wheel", "setuptools"))
-  system2(py, c("-m", "pip", "install", "--upgrade", "--no-user", "guildai"))
+  pip_install <- function(...)
+    system2(py, c("-m", "pip", "install", "--upgrade", "--no-user", ...))
+  pip_install("pip", "wheel", "setuptools")
+  pip_install(if(is.null(guildai)) "guildai" else c('-e', guildai))
   normalizePath(file.path(venv, "bin", "guild"))
 }
 
+# install_guild("/usr/bin/python3", "~/guild/guildai")
+
+
 find_guild <- function() {
-  if(file.exists(guild <- Sys.which("guild")))
-    return(normalizePath(as.vector(guild)))
   if(file.exists(guild <- file.path(rappdirs::user_data_dir("r-guildai"), "bin", "guild")))
+    return(normalizePath(as.vector(guild)))
+  if(file.exists(guild <- Sys.which("guild")))
     return(normalizePath(as.vector(guild)))
   install_guild()
 }
