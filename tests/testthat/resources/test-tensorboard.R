b36d7f79fc511a34d571d3027bdbb1e467e69d9c
Tomasz Kalinowski <tomasz@rstudio.com>
2022-09-29 13:22:08 -0400

    fix test

--- tests/testthat/resources/test-tensorboard.R
+++ tests/testthat/resources/test-tensorboard.R
@@ -5,10 +5,11 @@ library(tensorflow)
 
 writer = tf$summary$create_file_writer("a-events-file2")
 
-with writer.as_default():
-  for step in range(100):
+with(writer$as_default(), {
+
+  for(step in seq(10))
+    tf$summary$scalar("my_metric", 0.5, step=step)
     # other model code would go here
-    tf.summary.scalar("my_metric", 0.5, step=step)
+})
 
 
-tf$summary$create_
