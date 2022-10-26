8d43ba57566b006c54df9075ef1301275c19adae
Tomasz Kalinowski <tomasz@rstudio.com>
2022-10-26 17:09:51 -0400

    improve `ls_runs()` return df
    now more convenient for dplyr verbs

--- R/runs.R
+++ R/runs.R
@@ -2,75 +2,66 @@
 
 #' list guild runs
 #'
-#' @param full Whether to include all the available run info.
-#'
 #' @param ... additional arguments passed to `guild api runs`. Try
 #'   `"--help"` to see options.
 #'
 #' @return a dataframe of runs
 #' @export
 #' @importFrom jsonlite parse_json
-ls_runs <- function(..., full = FALSE) {
+#' @importFrom rlang %|%
+ls_runs <- function(...) {
   if ("--help" %in% c(...))
     return(guild("api runs --help"))
 
-  # --json option always shows all runs
-  x <- guild("api runs", ..., stdout = TRUE)
-  x <- paste0(x, collapse = '')
-  df <- parse_json(x, simplifyVector = TRUE)
+  df <- guild("api runs", ..., stdout = TRUE) |>
+    paste0(collapse = "") |>
+    parse_json(simplifyVector = TRUE)
 
   if(identical(df, list()))
     return()
 
-  # TODO: guild should return something that's tz aware,
-  #   This approach the best we can do is assume the dt string is
-  #   in the user locale
-  for(dt_name in c("started", "stopped"))
-    df[[dt_name]] <- as.POSIXct(df[[dt_name]],
-                                format = "%Y-%m-%d %H:%M:%OS")
-  # df$time <- as.difftime(df$time, units = "secs")
 
+  # drop some overly verbose info. All this
+  # is easily accessible in run_dir/.guild/attrs users that need it.
+  #
+  df[c("shortId", "time", "command", "files",
+       "env", "sourcecode", "opRef")] <- NULL
+
+  # additional coersion
   df$comments <- lapply(df$comments, as.character)
 
+
+  ## `guild api runs` is missing some info, mainly a portable timestamp
+  ## and marked status.
+  # --json option always shows all runs, so don't pass `...`
+  df2 <- guild("runs list --json", stdout = TRUE) |>
+    paste0(collapse = "") |>
+    parse_json(simplifyVector = TRUE)
+
+  df2 <- df2[match(df$id, df2$id), ]
+
+  stopifnot(identical(df$id, df2$id))
+
+  browser()
+
+  # use epoch for timestamps
+  df$started <- .POSIXct(df2$started/1000000)
+  df$stopped <- .POSIXct(df2$stopped/1000000)
+  df$marked <- df2$marked %|% FALSE
+
+
   # reorder columns for nicer printing.
-  nms <- unique(c("shortId", "label", "flags", "scalars", names(df)))
-  nms <- unique(c(nms, "env", "id"), fromLast = TRUE)
+  nms <- unique(c("label", "tags", "marked", "scalars", "flags",
+                  names(df)))
+  nms <- unique(c(nms, "dir", "id"), fromLast = TRUE)
   df <- df[nms]
 
-  df$scalars <- lapply(df$scalars, tibble::as_tibble)
+  ## fix up scalars to make it convenient for other dplyr verbs
 
-  # these columna are dataframes
-  for(nm in c("flags"))
-    df[[nm]] <- tibble::as_tibble(df[[nm]])
-
-  # these columns are lists of dataframes
-  for(nm in c("scalars", "files"))
-    df[[nm]] <- lapply(df[[nm]], tibble::as_tibble)
-
-
-  # df <- rapply(list(df), tibble::as_tibble,
-  #              classes = "data.frame", how = "replace")[[1L]]
-
-
-  if (isFALSE(full)) {
-    # delete not useful columns
-    df$opRef <- NULL
-    df$command <- NULL
-    df$env <- NULL
-    df$files <- NULL
-
-    # pluck just the relevant info
-    # df$sourcecode <- df$sourcecode$files
-    df$sourcecode <- NULL
-  }
 
   df <- tibble::as_tibble(df)
-
-  # df$scalars[[1]]
-  #   $prefix
-  #     observed ".guild" for stdout, "logs/train" "logs/validate" for tfevents
-  #   $tag
-  #     observed "loss",  "epoch_accuracy", etc.
+  df[["flags"]] <- tibble::as_tibble(df[["flags"]])
+  df[["scalars"]] <- lapply(df[["scalars"]], tibble::as_tibble)
 
   df
 }
