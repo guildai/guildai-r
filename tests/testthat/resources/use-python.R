3769e5c0c9133c8a847b4dac812d32ef718c9dc9
Tomasz Kalinowski <tomasz@rstudio.com>
2023-01-13 11:35:43 -0500

    enables tests for 'guild run <batch-R-op> -> R -> python' everywhere

--- tests/testthat/resources/use-python.R
+++ tests/testthat/resources/use-python.R
@@ -3,7 +3,11 @@
 
 n <- 1
 
-res <- system2("python",
+python <- Sys.which("python")
+if(!nzchar(python))
+  python <- Sys.which("python3")
+
+res <- system2(python,
                c("-c", shQuote(sprintf("print(%i+1)", n))), stdout = TRUE)
 stopifnot(identical(res, as.character(n + 1)))
 
--- tests/testthat/test-ops-with-r-and-python.R
+++ tests/testthat/test-ops-with-r-and-python.R
@@ -4,25 +4,27 @@
 test_that("VIRTUAL_ENV and other python cruft doesn't interfere with guild", {
 
   local_project(test_resource("use-python.R"))
-  # browser(); Sys.setenv(DEBUGR=1)
+  # browser();
+  # Sys.setenv(DEBUGR=1)
 
   if(!nzchar(Sys.which("python")) && !nzchar(guildai:::find_python()))
-    withr::local_path(guildai:::find_python())
+      withr::local_path(guildai:::find_python())
 
-  skip_if(!nzchar(Sys.which("python")), message = "no python on PATH")
+  if(!nzchar(Sys.which("python")) && !nzchar(Sys.which("python3")))
+    skip("no python on PATH")
 
   expect_single_run_succeedes <- function(info = NULL) {
     guild_run("use-python.R")
     expect_identical(runs_info(1L)$exit_status, 0L,
                      info = info)
-    guild("runs delete", stderr = FALSE)
+    guild("runs delete --yes", stderr = FALSE)
   }
 
   expect_batch_run_succeedes <- function(info = NULL) {
     guild_run("use-python.R", flags = list(n = 1:3))
     expect_identical(runs_info(1:3)$exit_status, c(0L, 0L, 0L),
                      info = info)
-    guild("runs delete", stderr = FALSE)
+    guild("runs delete --yes", stderr = FALSE)
   }
 
   do_test_batch <- TRUE
@@ -42,14 +44,14 @@ test_that("VIRTUAL_ENV and other python cruft doesn't interfere with guild", {
     writeLines('raise Exception("I should not be evaluated")', file.path("click", fi))
   }
 
-  do_test_batch <- is_windows() || local({
-    # ._pth files only work correctly in Python >= 3.11 on non-Windows
-    shebang <- readLines(guildai:::find_guild(), 1)
-    python <- str_drop_prefix(shebang, "#!")
-    py_ver <- system(paste(python, "--version"), intern = TRUE)
-    py_ver <- numeric_version(str_drop_prefix(py_ver, "Python "))
-    isTRUE(py_ver >= "3.11")
-  })
+  # do_test_batch <- is_windows() || tryCatch(local({
+  #   # ._pth files only work correctly in Python >= 3.11 on non-Windows
+  #   shebang <- readLines(guildai:::find_guild(), 1)
+  #   python <- str_drop_prefix(shebang, "#!")
+  #   py_ver <- system(paste(python, "--version"), intern = TRUE)
+  #   py_ver <- numeric_version(str_drop_prefix(py_ver, "Python "))
+  #   isTRUE(py_ver >= "3.11")
+  # }), error = function(e) TRUE)
 
   expect_run_succeedes("presence of things that look like modules in cwd")
 
@@ -60,7 +62,9 @@ test_that("VIRTUAL_ENV and other python cruft doesn't interfere with guild", {
 
   # Test user activated virtual_env doesn't interfere w/
   # direct call to guild executable
-  system2("python", c("-m", "venv", "my-venv"))
+  python <- Sys.which("python")
+  if(!nzchar(python)) python <- Sys.which("python3")
+  system2(python, c("-m", "venv", "my-venv"))
 
   # "activate" the venv.
   bin_path <- if(is_windows()) "my-venv/Scripts" else "my-venv/bin"
@@ -73,8 +77,8 @@ test_that("VIRTUAL_ENV and other python cruft doesn't interfere with guild", {
     expect_run_succeedes("PYTHONPATH set")
   })
 
-  if(!do_test_batch)
-    skip("user set PYTHON* vars leak into guild_python_exe runtime on this platform")
+  # if(!do_test_batch)
+  #   skip("user set PYTHON* vars leak into guild_python_exe runtime on this platform")
 
   # skip("user set PYTHONHOME leaks into guild_python_exe runtime")
   # local_envvar(PYTHONHOME = Sys.getenv("VIRTUAL_ENV"))
